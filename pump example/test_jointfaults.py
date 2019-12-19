76ee965146c1b07348bfaf59253e4dbdfec6a4c7
Daniel Hulse <hulsed@oregonstate.edu>
2019-12-19 15:31:08 -0800

    rates now added for jointfaults in SampleApproach

--- pump example/test_jointfaults.py
+++ pump example/test_jointfaults.py
@@ -16,9 +16,9 @@ from ex_pump import * #required to import entire module
 mdl = Pump(params={'water'}) # should give identical utilities
 mdl = Pump()
 
-app_jf1 = SampleApproach(mdl, jointfaults={'faults':2})
+app_jf1 = SampleApproach(mdl, jointfaults={'faults':2, 'pcond':0.1})
 # if a function can have multiple modes injected at the same time
-app_jf2 = SampleApproach(mdl, jointfaults={'faults':3, 'jointfuncs':True})
-app_jf5 = SampleApproach(mdl, jointfaults={'faults':5})
+app_jf2 = SampleApproach(mdl, jointfaults={'faults':3, 'jointfuncs':True, 'pcond':0.1})
+app_jf5 = SampleApproach(mdl, jointfaults={'faults':5, 'pcond':0.1})
 
-app_list = SampleApproach(mdl, jointfaults={'faults':[('ImportEE', 'inf_v'),('ImportWater', 'no_wat')]})
+app_list = SampleApproach(mdl, jointfaults={'faults':[(('ImportEE', 'inf_v'),('ImportWater', 'no_wat'))], 'pcond':0.1})
