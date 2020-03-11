15b9c7b9e24ab081056e09331d4007dbc743382b
Daniel Hulse <hulsed@oregonstate.edu>
2020-03-11 13:54:04 -0700

    More tank model setup
    Added action sequence graph as function/components

--- two tank system/tank_model.py
+++ two tank system/tank_model.py
@@ -22,12 +22,12 @@ Information in Engineering Conference. American Society of Mechanical Engineers
 Digital Collection.
 """
 
-from fmdtools.modeldef import Model, FxnBlock
+from fmdtools.modeldef import Model, FxnBlock, Component
 
 
 class ImportLiquid(FxnBlock):
     def __init__(self,flows):
-        super().__init__(['Watout'],flows,{'open':1.0})
+        super().__init__(['Watout', 'Sig'],flows,{'open':1.0})
         self.assoc_modes({'Stuck'}) # need to add human-induced?
     def behavior(self,time):
         #if self.time > 10.0: self.open = 0.0
@@ -36,7 +36,7 @@ class ImportLiquid(FxnBlock):
 
 class ExportLiquid(FxnBlock):
     def __init__(self,flows):
-        super().__init__(['Watin'],flows,{'open':1})
+        super().__init__(['Watin', 'Sig'],flows,{'open':1})
         self.assoc_modes({'Stuck'}) # need to add human-induced?
     def behavior(self,time):
         if self.time > 10.0: self.open = 0.0
@@ -57,11 +57,10 @@ class GuideLiquid(FxnBlock):
         else:
             self.Watout.effort = self.Watin.effort
             self.Watin.rate = self.Watout.rate     
-
     
 class StoreLiquid(FxnBlock):
     def __init__(self,flows):
-        super().__init__(['Watin','Watout'],flows, {'level':5.0, 'net_flow': 0.0})
+        super().__init__(['Watin','Watout', 'Sig'],flows, {'level':5.0, 'net_flow': 0.0})
         self.assoc_modes({'Leak'})
     def behavior(self, time):
         if self.level >= 10.0:
@@ -79,6 +78,74 @@ class StoreLiquid(FxnBlock):
         else:                       self.net_flow = self.Watin.rate - self.Watout.rate
         
         if time>self.time:          self.level = self.level + self.net_flow
+        
+class HumanActions(FxnBlock):
+    def __init__(self,flows):
+        actions = {'look':Look('Look'),'detect':Detect('Detect'),\
+                   'reach':Reach('Reach'), 'grasp':Grasp('Grasp'),\
+                   'turn':Turn('Turn')}
+        super().__init__(['Valve_Sig','Tank_Sig', 'OtherValve_Sig'], flows, components=actions,timers={'t1'})
+    def behavior(self,time):
+        for actname, action in self.components.items(): # maybe this component iteration should be built into the class?
+            for fault in self.faults:
+                if fault in action.faultmodes:  action.faults.update([fault])
+        
+        if time > self.time:
+            if self.t1.t == 0:
+                self.look = self.components.look.behavior()
+                self.detect = self.components.detect.behavior(self.look,self.Valve_Sig.indicator)
+                if self.detect: self.t1.inc(1)
+            elif self.t1.t < 10: self.t1.inc(1)
+            elif self.t1.t >=10:
+                self.reach = self.components.reach.behavior()
+                self.grasp = self.components.grasp.behavior()
+                self.turn  = self.components.turn.behavior(self.grasp, self.Valve_Sig.indicator)
+                if self.reach[0]:
+                    self.Valve_Sig.action = self.turn
+                if self.reach[1]:
+                    self.Valve_Sig.action = self.turn
+                self.t1.reset()
+            
+class Look(Component):
+    def __init__(self, name):
+        super().__init__(name)
+        self.assoc_modes({'NotVisible'})
+    def behavior(self):
+        if self.has_fault('NotVisible'):    return 0
+        else:                               return 1
+
+class Detect(Component):
+    def __init__(self,name):
+        super().__init__(name)
+        self.assoc_modes({'NotDetected'})
+    def behavior(self, look, signal):
+        if self.has_fault('NotDetected'):   detect = 0
+        else:                               detect = look * signal
+        return detect
+class Reach(Component):
+    def __init__(self,name):
+        super().__init__(name)
+        self.assoc_modes({'FalseReach', 'CannotReach'})
+    def behavior(self):
+        if self.has_fault('CannotReach'):   return 0,0
+        elif self.has_fault('FalseReach'):  return 0,1
+        else:                               return 1,0
+class Grasp(Component):
+    def __init__(self,name):
+        super().init__(name)
+        self.assoc_modes({'CannotGrasp'})
+    def behavior(self):
+        if self.has_fault('CannotGrasp'):   return 0
+        else:                               return 1
+class Turn(Component):
+    def __init__(self,name):
+        super().init__(name)
+        self.assoc_modes({'CannotTurn'})
+    def behavior(self,grasp, intended_turn):
+        if self.has_fault('CannotTurn') or grasp == 0:  return 0
+        else:                                           return intended_turn
+
+
             
 class Tank(Model):
     def __init__(self, params={}):
@@ -88,12 +155,16 @@ class Tank(Model):
         self.add_flow('Wat_in_2', 'Water', {'effort':1.0, 'rate':1.0})
         self.add_flow('Wat_out_1', 'Water', {'effort':1.0, 'rate':1.0})
         self.add_flow('Wat_out_2', 'Water', {'effort':1.0, 'rate':1.0})
+        self.add_flow('Valve1_Sig', 'Signal', {'indicator':1.0, 'action':1.0})
+        self.add_flow('Tank_Sig', 'Signal', {'indicator':1.0, 'action':1.0})
+        self.add_flow('Valve2_Sig', 'Signal', {'indicator':1.0, 'action':1.0})
         
-        self.add_fxn('Import_Water', ImportLiquid, ['Wat_in_1'])
+        self.add_fxn('Import_Water', ImportLiquid, ['Wat_in_1', 'Valve1_Sig'])
         self.add_fxn('Guide_Water_In', GuideLiquid, ['Wat_in_1', 'Wat_in_2'])
-        self.add_fxn('Store_Water', StoreLiquid, ['Wat_in_2', 'Wat_out_1'])
+        self.add_fxn('Store_Water', StoreLiquid, ['Wat_in_2', 'Wat_out_1', 'Tank_Sig'])
         self.add_fxn('Guide_Water_Out', GuideLiquid, ['Wat_out_1', 'Wat_out_2'])
-        self.add_fxn('Export_Water', ExportLiquid, ['Wat_out_2'])
+        self.add_fxn('Export_Water', ExportLiquid, ['Wat_out_2', 'Valve2_Sig'])
+        self.add_fxn('Human', HumanActions, ['Valve1_Sig', 'Tank_Sig', 'Valve2_Sig'])
         
         self.construct_graph()
         
