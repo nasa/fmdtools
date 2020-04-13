dec9d8b0c661668e2b4c383935353747e8b168d7
Daniel Hulse <hulsed@oregonstate.edu>
2020-04-13 12:51:02 -0700

    Added ability to choose graph type in networks
    - get_graph now gets the appropriate graph type from mdl
    - methods now work with both models and graphs

--- fmdtools/faultsim/networks.py
+++ fmdtools/faultsim/networks.py
@@ -18,50 +18,53 @@ import math
 
 
 # Network Metric Quantification
-def calc_aspl(mdl):
+def calc_aspl(mdl, gtype='parameter'):
     """
         Computes average shortest path length of graph representation of model mdl.
         
         Parameters
         ----------
-        mdl : model
+        mdl : model or graph
         
         Returns
         -------
         ASPL : average shortest path length
         """
-    g = mdl.graph
+    if type(mdl)==nx.classes.graph.Graph:   g = mdl
+    else:                                   g = get_graph(mdl, gtype)
     ASPL = nx.average_shortest_path_length(g)
     return ASPL
-def calc_modularity(mdl):
+def calc_modularity(mdl, gtype='parameter'):
     """
         Computes graph modularity given a graph representation of model mdl.
         
         Parameters
         ----------
-        mdl : model
+        mdl : model or graph
         
         Returns
         -------
         modularity : Modularity
         """
-    g = mdl.graph
+    if type(mdl)==nx.classes.graph.Graph:   g = mdl
+    else:                                   g = get_graph(mdl, gtype)
     communities = list(greedy_modularity_communities(g))
     m = modularity(g,communities)
     return m
-def find_bridging_nodes(mdl,plot='off'):
+def find_bridging_nodes(mdl,plot='off', gtype = 'parameter'):
     """
         Determines bridging nodes in a graph representation of model mdl.
         
         Parameters
         ----------
-        mdl : model
+        mdl : model or graph
         
         Returns
         -------
         bridgingNodes : list of bridging nodes
         """
-    g = mdl.graph
+    if type(mdl)==nx.classes.graph.Graph:   g = mdl
+    else:                                   g = get_graph(mdl, gtype)
     communitiesRaw = list(greedy_modularity_communities(g))
     communities = [list(x) for x in communitiesRaw]
     numCommunities = len(communities)
@@ -94,13 +97,13 @@ def find_bridging_nodes(mdl,plot='off'):
         plt.title('Bridging Nodes')
         plt.show()
     return bridgingNodes
-def find_high_degree_nodes(mdl,p=.1,plot='off'):
+def find_high_degree_nodes(mdl,p=.1,plot='off', gtype='bipartite'):
     """
         Determines highest degree nodes, up to percentile p, in graph representation of model mdl.
         
         Parameters
         ----------
-        mdl : model
+        mdl : model or graph
         p : percentile of degrees to return, between 0 and 1
         plot : plots graph with high degree nodes visualized if set to 'on'
         
@@ -108,7 +111,8 @@ def find_high_degree_nodes(mdl,p=.1,plot='off'):
         -------
         highDegreeNodes : list of high degree nodes in format (node,degree)
         """
-    g = mdl.graph
+    if type(mdl)==nx.classes.graph.Graph:   g = mdl
+    else:                                   g = get_graph(mdl, gtype)
     d = list(g.degree())
     def take_second(elem):
         return elem[1]
@@ -138,20 +142,21 @@ def find_high_degree_nodes(mdl,p=.1,plot='off'):
         plt.title('High Degree Nodes')
         plt.show()
     return highDegreeNodes
-def calc_robustness_coefficient(mdl,trials=100):
+def calc_robustness_coefficient(mdl,trials=100, gtype='bipartite'):
     """
         Computes robustness coefficient of graph representation of model mdl.
         
         Parameters
         ----------
-        mdl : model
+        mdl : model or graph
         trials : number of times to run robustness coefficient algorithm (result is averaged over all trials)
         
         Returns
         -------
         RC : robustness coefficient
         """
-    g = mdl.graph
+    if type(mdl)==nx.classes.graph.Graph:   g = mdl
+    else:                                   g = get_graph(mdl, gtype)
     trialsRC = list()
     for itr in range(trials):
         tmp = g.copy()
@@ -167,19 +172,20 @@ def calc_robustness_coefficient(mdl,trials=100):
         trialsRC.append((200*sum(s)-100*s[0])/N/N)
     RC = sum(trialsRC)/len(trialsRC)
     return RC
-def degree_dist(mdl):
+def degree_dist(mdl, gtype='bipartite'):
     """
         Plots degree distribution of graph representation of model mdl.
         
         Parameters
         ----------
-        mdl : model
+        mdl : model or graph
         
         Returns
         -------
         
         """
-    g = mdl.graph
+    if type(mdl)==nx.classes.graph.Graph:   g = mdl
+    else:                                   g = get_graph(mdl, gtype)
     degrees = [g.degree(n) for n in g.nodes()]
     degreesSet = set(degrees)
     degreesUnique = list(degreesSet)
@@ -198,3 +204,11 @@ def degree_dist(mdl):
     plt.xlabel('Degree')
     plt.ylabel('Frequency')
     plt.show()
+
+def get_graph(mdl, gtype):
+    "gets the appropriate graph of type gtype from mdl"
+    if gtype == 'normal':       g = mdl.graph
+    elif gtype == 'bipartite':  g=mdl.bipartite
+    elif gtype == 'parameter':  g = mdl.return_paramgraph()
+    elif gtype == 'component':  g = mdl.return_stategraph(gtype='component')
+    return g
