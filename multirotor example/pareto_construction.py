29ec808ae36a27c1208b16f954b580324f26ba86
Daniel Hulse <hulsed@oregonstate.edu>
2020-10-06 21:21:40 -0700

    added methods for 2d/2d pareto finding, added to notebook

--- multirotor example/pareto_construction.py
+++ multirotor example/pareto_construction.py
@@ -8,29 +8,73 @@ Created on Tue Oct  6 12:57:40 2020
 from drone_opt import *
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 #results = brute_search()
 
 #resultstab = pd.DataFrame(results)
-
-#resultstab.to_csv('fullspace.csv')
 #resultstab.columns = resultstab.columns.to_flat_index()
+#resultstab.to_csv('fullspace.csv')
+
 #resultstab = pd.read_csv('fullspace.csv')
+
+
+
+# TWO 3-D PARETO FRONTS:
+#   - Design, Operational, Resilience Cost 
+#   - Repair, Landing, Flight Cost (Safety - Viewed Value)
 
 resultstab = pd.read_csv('fullspace.csv')
 resultstab = resultstab.drop(labels='Unnamed: 0', axis=1)
 
-pareto = dict()
+pareto3 = get_3dpareto(resultstab, 0,1,2)
 
-for x in resultstab:
-    if not any([(resultstab[x][0] >= resultstab[i][0] and resultstab[x][2] > resultstab[i][2]) or (resultstab[x][0] > resultstab[i][0] and resultstab[x][2] >= resultstab[i][2]) for i in resultstab]):
-        pareto[x] = resultstab[x][1], resultstab[x][2]
+x = [x for x,y,z in pareto3.values()]
+y = [y for x,y,z in pareto3.values()]
+z = np.array([z for x,y,z in pareto3.values()])
 
-pareto = dict(sorted(pareto.items(), key=lambda x: x[1]))
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x,y,z)
+#ax.plot_trisurf(x,y,z)
+ax.set_xlabel('DesignCost')
+ax.set_ylabel('Operational Cost')
+ax.set_zlabel('Resilience Cost')
+
+resilcosts = resultstab[3:5]
+flightcosts = resultstab[5:7].sum()
+resilcosts = resilcosts.append(flightcosts, ignore_index=True)
+
+pareto3 = get_3dpareto(resilcosts, 0,1,2)
+
+x = [x for x,y,z in pareto3.values()]
+y = [y for x,y,z in pareto3.values()]
+z = np.array([z for x,y,z in pareto3.values()])
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x,y,z)
+#ax.plot_trisurf(x,y,z)
+ax.set_xlabel('Repair Cost')
+ax.set_ylabel('Landing Cost')
+ax.set_zlabel('Flight Cost')
+
+
+
+
+plt.figure()
+pareto = get_2dpareto(resultstab, 5, 6)
 
 x = [x for x,y in pareto.values()]
 y = [y for x,y in pareto.values()]
 
 plt.plot(x,y)
-plt.xlabel('Design Cost')
-plt.ylabel('Resilience Cost')
+plt.xlabel('Safety Cost')
+plt.ylabel('Lost Value')
+
+
+
+
+
+
