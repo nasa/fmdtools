c4ca70b5ac3b7e134bd32db4c7adb2e4c6d94792
Daniel Hulse <hulsed@oregonstate.edu>
2021-02-05 11:03:17 -0800

    implementing EA on tank problem

--- hold-up tank example/optimization version/tank_opt.py
+++ hold-up tank example/optimization version/tank_opt.py
@@ -11,6 +11,7 @@ import numpy as np
 import scipy as sp
 import time
 import itertools
+import random
 
 import fmdtools.faultsim.propagate as propagate
 import fmdtools.resultdisp as rd
@@ -46,6 +47,37 @@ def x_to_rcost2(xres):
     endclasses, mdlhists = propagate.approach(mdl, app, staged=True)
     rescost = rd.process.totalcost(endclasses)
     return rescost
+def x_to_rcost3(xres1,xres2):
+    fp = {(a-1,b-1,c-1):(xres1,xres2) for i,(a,b,c) in enumerate(np.ndindex((3,3,3)))}
+    mdl=Tank(params={'capacity':20,'turnup':1,'faultpolicy':fp})
+    app = SampleApproach(mdl)
+    endclasses, mdlhists = propagate.approach(mdl, app, staged=True)
+    rescost = rd.process.totalcost(endclasses)
+    return rescost
+
+def EA(popsize=10, iters=10):
+    starttime = time.time()
+    pop = seedpop() + randpop(popsize-3)
+    
+    for i in range(iters):
+        values = [x_to_rcost(x[0],x[1]) for x in pop]
+        goodpop, goodvals = select(pop, values)
+        
+    
+    return results, opt_hist, len(Xvals), time.time() - starttime
+def randpop(popsize):
+    return np.array([[[random.randint(-1,1) for a in range(0,27)],[random.randint(-1,1) for a in range(0,27)]] for i in range(0,popsize)])
+def seedpop():
+    donothing = np.zeros((2,27))
+    adjustup = np.ones((2,27))
+    adjustdown = -np.ones((2,27))
+    return np.array([donothing, adjustup, adjustdown])
+def permute(solution):
+    solution[[random.randint(0,27)][random.randint(0,1)]]=random.randint(-1,1)
+    return solution
+def select(solutions, values, quantile=0.5):
+    selection = [np.quantile(solutions,quantile)>solutions]
+    return solutions[selection], values[selection]
 
 def brute_search(): 
     #uh oh--the space of the lower level problem is way too big for an exhaustive search!
@@ -59,6 +91,8 @@ def brute_search():
         elif rescost < opt_hist[-1][0]:     opt_hist.append([rescost, X])
             
     return results, opt_hist, len(Xvals), time.time() - starttime
+
+
 
 
 
