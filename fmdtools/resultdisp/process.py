Fixed process.average bug for NaN quantities

fmdtools/resultdisp/process.py
@@ -469,7 +469,7 @@ def expected(endclasses, metric):
     return sum([e[metric]*e['rate'] for k,e in endclasses.items() if not np.isnan(e[metric])])
 def average(endclasses, metric):
     """Calculates the average value of a given metric in endclasses"""
-    return sum([e[metric] for k,e in endclasses.items() if not np.isnan(e[metric])])/len(endclasses)
+    return np.mean([e[metric] for k,e in endclasses.items() if not np.isnan(e[metric])])
 def percent(endclasses, metric):
     """Calculates the percentage of a given indicator variable being True in endclasses"""
     return sum([int(bool(e[metric])) for k,e in endclasses.items() if not np.isnan(e[metric])])/len(endclasses)