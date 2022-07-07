814d112ffcdef8ea2b87f4619621c3287e6a4718
Daniel Hulse <daniel.e.hulse@nasa.gov>
2022-07-07 11:12:23 -0700

    Update version

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ project = 'fmdtools'
 author = 'Daniel Hulse, Sequoia Andrade, Hannah Walsh, Lukman Irshad'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
