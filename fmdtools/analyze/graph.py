74524a7a516ac36b0e23898e14ce4bc39dcd4140
Daniel Hulse <daniel.e.hulse@nasa.gov>
2023-04-22 16:46:05 -0700

    More organization work
    - networks.py now merged into analyze.graph
    - some additional fixes around result moving to analyze
    - starting to add things for graphing at particular times

--- fmdtools/analyze/graph.py
+++ fmdtools/analyze/graph.py
@@ -13,14 +13,16 @@ Private class:
     - :class:`GraphInteractor`:             Used to set nodes in set_pos
 """
 #File Name: analyze/graph.py
-#Contributors: Daniel Hulse and Sequoia Andrade
+#Contributors: Daniel Hulse, Sequoia Andrade, Hannah Walsh
 #Created: November 2019
 #Refactored: April 2020
 #Added major interfaces: July 2021
+#Adopted object-oriented architecture and merged in networks.py: April 2023
 
 
 import networkx as nx
 import numpy as np
+from numpy.random import random
 import matplotlib.pyplot as plt
 plt.rcParams['pdf.fonttype'] = 42 
 import matplotlib.animation
@@ -28,7 +30,8 @@ from matplotlib.patches import Patch
 from matplotlib.widgets import Button
 from matplotlib import get_backend
 from recordclass import dataobject, asdict
-from copy import copy
+import copy
+from .result import Result, History
 
 
 default_edge_kwargs={'sends':       dict(edge_color='grey', style='dashed'),
@@ -70,7 +73,7 @@ default_node_kwargs={'Model':       dict(node_shape='^'),
                      'MultiFlow':   dict(node_shape='h'),
                      'CommsFlow':   dict(node_shape='8'),
                      'State':       dict(node_shape='d'),
-                     'actiive':     dict(node_color='orange'),
+                     'active':     dict(node_color='orange'),
                      'degraded':    dict(node_color='orange'),
                      'faulty':      dict(node_color='red')}  
 
@@ -381,7 +384,7 @@ class Graph(object):
         
         if title: plt.title(title)
         return fig
-    def move_nodes(self, g, gtype='fxnflowgraph', **kwargs):
+    def move_nodes(self, g, **kwargs):
         """
         Sets the position of nodes for plots in analyze.graph using a graphical tool.
         Note: make sure matplotlib is set to plot in an external window (e.g using '%matplotlib qt)
@@ -390,8 +393,6 @@ class Graph(object):
         ----------
         g : networkx graph or model or function
             fxngraph or fxnflowgraph graph of the model of interest
-        gtype : 'fxngraph' or 'fxnflowgraph', optional
-            Type of graph to plot. The default is 'fxngraph'.
         **kwargs : kwargs
             keyword arguments for graph.draw
     
@@ -406,10 +407,398 @@ class Graph(object):
             print("Cannot place nodes in inline version of plot. Use '%matplotlib qt' (or '%matplotlib osx') to open in external window")
         return p
     def set_degraded(self, other):
+        """
+        Sets 'degraded' state in underlying networkx graph based on difference between
+        states with another Graph object
+
+        Parameters
+        ----------
+        other : Graph
+            (assumed nominal) Graph to compare to
+        """
         g = self.g 
         nomg = other.g
         for node in g.nodes:   
             g.nodes[node]['degraded'] = g.nodes[node]['states']!=nomg.nodes[node]['states']
+    def draw_state_from(self, time, history=History(), degraded=History(), **kwargs):
+        faulty = history.get_faulty_hist(*self.nodes)
+        fault_nodes = faulty.get_slice(time)
+        deg_nodes = degraded.get_slice(time)
+        state_nodes = history.get(*self.nodes)
+        nx.set_node_attributes(self.g, fault_nodes, 'faulty')
+        nx.set_node_attributes(self.g, deg_nodes, 'degraded')
+        nx.set_node_attributes(self.g, state_nodes, 'states')
+        self.draw(**kwargs)
+    def draw_pyvis(self, filename="graph", width=1000, filt=True, physics=False, notebook=False):
+        """
+        Method for plotting graphs with pyvis. Produces interactive HTML!
+
+        Parameters
+        ----------
+        g : networkx graph or model
+            Graph to plot or fmdtools model (which will be used to get the graph)
+        gtype : 'hierarchical'/'fxnflowgraph'/'component', optional
+            Type of model graph to plot The default is 'hierarchical'.
+        filename : str, optional
+            File to save the html to. The default is "typegraph.html".
+        width : int, optional
+            Width of the frame in px. The default is 1000.
+        filt : Dict/Bool, optional
+            Whether to display sliders. The default is True.
+        physics : Bool, optional
+            Whether to use physics during node placement. The default is False.
+        Returns
+        -------
+        n : pyvis object
+            pyvis object of the drawn graph
+        """
+        from pyvis.network import Network
+        width = str(width)+"px"
+
+        if isinstance(self, ModelTypeGraph): 
+            n = Network(directed=True, layout='hierarchical', width=width, notebook=notebook)
+        else:
+            n = Network(width=width, notebook=notebook)
+        n.from_nx(self.g)
+        n.toggle_physics(physics)
+        if filt: n.show_buttons(filter_=filt)
+        n.show(filename+".html")
+        return n
+    def calc_aspl(self):
+        """
+        Computes average shortest path length of
+
+        Returns
+        -------
+        aspl: float
+            Average shortest path length
+        """
+        return nx.average_shortest_path_length(self.g)
+    def calc_modularity(self):
+        """
+        Computes network modularity of the graph.
+            
+        Returns
+        -------
+        modularity : Modularity
+        """
+        from networkx.algorithms.community import greedy_modularity_communities
+        from networkx.algorithms.community.quality import modularity
+        communities = list(greedy_modularity_communities(self.g))
+        return  modularity(self.g,communities) 
+    def find_bridging_nodes(self):
+        """
+        Determines bridging nodes in a graph representation of model mdl. 
+        
+        Returns
+        -------
+        bridgingNodes : list of bridging nodes
+        """
+        from networkx.algorithms.community import greedy_modularity_communities
+        g=self.g
+        communitiesRaw = list(greedy_modularity_communities(g))
+        communities = [list(x) for x in communitiesRaw]
+        numCommunities = len(communities)
+        nodes = list(g.nodes)
+        numNodes = len(nodes)
+        bridgingNodes = list()
+        nodeEdges = list()
+        for i in range(0,numNodes):
+            nodeEdges.append(list(g.edges(nodes[i])))
+            lenNodeEdges = len(nodeEdges[i])
+            for j in range(numCommunities):
+                if nodes[i] in communities[j]:
+                    communityIdx = j
+            for j in range(lenNodeEdges):
+                nodeEdgePair = list(nodeEdges[i][j])
+                if nodeEdgePair[1] in communities[communityIdx]:
+                    pass
+                else:
+                    bridgingNodes.append(nodes[i])
+        bridgingNodes = sorted(list(set(bridgingNodes)))
+        return bridgingNodes 
+    def plot_bridging_nodes(self, title='bridging nodes', **kwargs):
+        """
+        Plots bridging nodes using self.draw()
+
+        Parameters
+        ----------
+        title : str, optional
+            Title for the plot. The default is 'bridging nodes'.
+        **kwargs : kwargs
+            kwargs for Graph.draw
+
+        Returns
+        -------
+        fig : matplotlib figure
+            Figure
+        """
+        bridgingnodes = self.find_bridging_nodes()
+        self.add_node_groups(bridging_nodes=bridgingnodes)
+        fig = self.draw(title=title, **kwargs)
+        return fig
+    def find_high_degree_nodes(self, p=90):
+        """
+        Determines highest degree nodes, up to percentile p, in graph representation of model mdl.
+        
+        Parameters
+        ----------
+        p : int (optional)
+            percentile of degrees to return, between 0 and 100
+        
+        Returns
+        -------
+        highDegreeNodes : list of high degree nodes in format (node,degree)
+        """
+        g= self.g
+        d = list(g.degree())
+        def take_second(elem):
+            return elem[1]
+        sortedNodes = sorted(d, key=take_second, reverse=True)
+        sortedDegrees = [x[1] for x in sortedNodes]
+        sortedDegreesSet = set(sortedDegrees)
+        sortedDegreesUnique = list(sortedDegreesSet)
+        sortedDegreesUniqueArray = np.array(sortedDegreesUnique)
+        topPercentileDegree = np.percentile(sortedDegreesUniqueArray,p)
+        numNodes = len(sortedNodes)
+        highDegreeNodes = [sortedNodes[0]]
+        for i in range(1,numNodes):
+            if sortedNodes[i][1] < topPercentileDegree:
+                pass
+            else:
+                highDegreeNodes.append(sortedNodes[i])
+        return highDegreeNodes
+    def plot_high_degree_nodes(self, p=90, title='', **kwargs):
+        """
+        Plots high-degree nodes using self.draw()
+
+        Parameters
+        ----------
+        p : int (optional)
+            percentile of degrees to return, between 0 and 100
+        title : str, optional
+            Title for the plot. The default is 'High Degree Nodes'.
+        **kwargs : kwargs
+            kwargs for Graph.draw
+
+        Returns
+        -------
+        fig : matplotlib figure
+            Figure
+        """
+        if not title: title='High Degree Nodes ('+str(p)+'th Percentile)'
+        hdnodes = self.find_high_degree_nodes()
+        self.add_node_groups(high_degree_nodes=hdnodes)
+        fig = self.draw(title=title, **kwargs)
+        return fig
+    def calc_robustness_coefficient(self, trials=100, seed=False):
+        """
+        Computes robustness coefficient of graph representation of model mdl.
+        
+        Parameters
+        ----------
+        trials : int 
+            number of times to run robustness coefficient algorithm (result is averaged over all trials)
+        seed : int
+            optional seed to instantiate test with
+        
+        Returns
+        -------
+        RC : robustness coefficient
+        """
+        g=self.g
+        if seed:    rng = np.random.default_rng(seed=seed)
+        else:       rng = np.random.default_rng()
+        
+        trialsRC = list()
+        for itr in range(trials):
+            tmp = g.copy()
+            N = float(len(tmp))
+            largestCC = max(nx.connected_components(tmp), key=len)
+            s = [float(len(largestCC))]
+            rs = rng.sample(range(int(s[0])),int(s[0]))
+            nodes = list(g)
+            for i in range(int(s[0])-1):
+                tmp.remove_node(nodes[rs[i]])
+                largestCC = max(nx.connected_components(tmp), key=len)
+                s.append(float(len(largestCC)))
+            trialsRC.append((200*sum(s)-100*s[0])/N/N)
+        RC = sum(trialsRC)/len(trialsRC)
+        return RC
+    def plot_degree_dist(self):
+        """
+        Plots degree distribution of graph representation of model mdl.
+        
+        Returns
+        -------
+        fig : matplotlib figure
+            plot of distribution
+        """
+        import math
+        g=self.g
+        degrees = [g.degree(n) for n in g.nodes()]
+        degreesSet = set(degrees)
+        degreesUnique = list(degreesSet)
+        freq = [degrees.count(n) for n in degreesUnique]
+        maxFreq = max(freq)
+        freqint = list(range(0,maxFreq+1))
+        degreeint = list(range(min(degrees),math.ceil(max(degrees))+1))
+        degreesSet = set(degrees)
+        degreesUnique = list(degrees)
+        numDegreesUnique = len(degreesUnique)
+        
+        fig = plt.figure()
+        plt.hist(degrees,bins=np.arange(numDegreesUnique)-0.5)
+        plt.xticks(degreeint)
+        plt.yticks(freqint)
+        plt.title('Degree distribution')
+        plt.xlabel('Degree')
+        plt.ylabel('Frequency')
+        plt.show()
+        return fig
+    def sff_model(self, endtime=5,pi=.1,pr=.1,num_trials=100,start_node='random',error_bar_option='off'):
+        """
+        susc-fix-fail model.
+        
+        Parameters
+        ----------
+        endtime: int
+            simulation end time
+        pi : float
+            infection (failure spread) rate
+        pf : float
+            recovery (fix) rate
+        num_trials : int 
+            number of times to run the epidemic model, default is 100
+        error_bar_option : str 
+            option for plotting error bars (first to third quartile), default is off
+        start_node : str
+            start node to use in the trial. default is 'random'
+        
+        Returns
+        -------
+        fig: plot of susc, fail, and fix nodes over time
+        
+        """
+        g= self.g
+        if start_node == 'random':
+            nodes = list(g.nodes)
+            start_node_selected= nodes[random.randint(0,len(nodes))]
+        else: start_node_selected=start_node
+        num_susc_all_trials = []
+        num_fail_all_trials = []
+        num_fix_all_trials = []
+        for trials in range(0,num_trials):
+       		num_susc_trial, num_fail_trial, num_fix_trial = sff_one_trial(start_node_selected,g,endtime=endtime,pi=pi,pr=pr)
+       		num_susc_all_trials.append(num_susc_trial)
+       		num_fail_all_trials.append(num_fail_trial)
+       		num_fix_all_trials.append(num_fix_trial)
+        num_susc_average = data_average(num_susc_all_trials)
+        num_fail_average = data_average(num_fail_all_trials)
+        num_fix_average = data_average(num_fix_all_trials)
+       
+        fig = plt.figure()
+        time_list = range(0,endtime+1)
+        if error_bar_option == 'on':
+       		num_susc_lower_error, num_susc_upper_error = data_error(num_susc_all_trials,num_susc_average)
+       		num_fail_lower_error, num_fail_upper_error = data_error(num_fail_all_trials,num_fail_average)
+       		num_fix_lower_error, num_fix_upper_error = data_error(num_fix_all_trials,num_fix_average)
+       		num_susc_asymmetric_error = [num_susc_lower_error, num_susc_upper_error]
+       		num_fail_asymmetric_error = [num_fail_lower_error, num_fail_upper_error]
+       		num_fix_asymmetric_error = [num_fix_lower_error, num_fix_upper_error]
+       		plt.errorbar(time_list,num_susc_average,yerr=num_susc_asymmetric_error,fmt='-o',label='Susceptible')
+       		plt.errorbar(time_list,num_fail_average,yerr=num_fail_asymmetric_error,fmt='-o',label='Failed')
+       		plt.errorbar(time_list,num_fix_average,yerr=num_fix_asymmetric_error,fmt='-o',label='Fixed')
+        else:
+       		plt.plot(time_list,num_susc_average,label='Susceptible')
+       		plt.plot(time_list,num_fail_average,label='Failed')
+       		plt.plot(time_list,num_fix_average,label='Fixed')
+        plt.legend()
+        plt.title('SFF model')
+        plt.xlabel('Time steps')
+        plt.ylabel('Number of nodes')
+        plt.show()
+        return fig
+
+def sff_one_trial(start_node_selected,g,endtime=5,pi=.1,pr=.1):
+    """
+    Calculates one trial of the sff model
+    
+    Parameters
+    ----------
+    start_node_selected : str
+        node to start the trial from
+    g : networkx graph
+        graph to run the trial over
+    endtime: int
+        simulation end time
+    pi : float
+        infection (failure spread) rate
+    pf : float
+        recovery (fix) rate
+    """
+    nodes = list(g.nodes)
+    num_nodes = len(nodes)
+    time = 0
+    susc_nodes = nodes
+    susc_nodes.remove(start_node_selected)
+    fail_nodes = [start_node_selected]
+    fix_nodes = []
+    num_susc = [num_nodes-1]
+    num_fail = [1]
+    num_fix = [0]
+    while time < endtime:
+        time = time + 1
+        new_exposed_nodes = []
+        for i in range(0,len(fail_nodes)):
+            n = list(g.neighbors(fail_nodes[i])) 
+            new_exposed_nodes.extend(n)
+        ri_list = [random.random() for iter in range(len(new_exposed_nodes))]
+        new_fail_nodes = []
+        for i in range(0,len(new_exposed_nodes)):
+            if new_exposed_nodes[i] in fix_nodes: pass
+            else:
+                if ri_list[i] <= pi:
+                    new_fail_nodes.append(new_exposed_nodes[i])
+        new_fail_nodes_set = set(new_fail_nodes)
+        new_fail_nodes = list(new_fail_nodes_set)
+        for i in range(0,len(new_fail_nodes)):
+            if new_fail_nodes[i] in fail_nodes: pass
+            else: susc_nodes.remove(new_fail_nodes[i])
+        fail_nodes.extend(new_fail_nodes)
+        fail_nodes_set = set(fail_nodes)
+        fail_nodes = list(fail_nodes_set)
+        rf_list = [random.random() for iter in range(len(fail_nodes))]
+        new_fix_nodes = []
+        for i in range(0,len(fail_nodes)):
+            if rf_list[i] <= pr:
+                new_fix_nodes.append(fail_nodes[i])
+        fix_nodes.extend(new_fix_nodes)
+        fail_nodes = list(set(fail_nodes)-set(new_fix_nodes))
+        num_susc.append(len(susc_nodes))
+        num_fail.append(len(fail_nodes))
+        num_fix.append(len(fix_nodes))
+    return num_susc, num_fail, num_fix
+
+def data_average(data):
+    """Averages each column in data"""
+    list_average = []
+    for i in range(0,len(data[0])):
+        list_average.append(sum(x[i] for x in data)/len(data))
+    return list_average
+
+def data_error(data,average):
+    """Calculates error for each column in data"""
+    q1 = []
+    q3 = []
+    for i in range(0,len(data[0])):
+        current_array = np.array([float(x[i]) for x in data])
+        q1.append(np.percentile(current_array,25))
+        q3.append(np.percentile(current_array,75))
+    lower_error = [x - y for x, y in zip(average,q1)]
+    upper_error = [x - y for x, y in zip(q3,average)]
+    return lower_error, upper_error
+        
 
 class GraphInteractor: 
     """A simple interactive graph for consistent node placement, etc--used in set_pos to set node positions"""
@@ -482,6 +871,7 @@ class GraphInteractor:
         plt.show()
     def print_pos(self):
         print({k:list(v) for k,v in self.pos.items()})
+    
 
 # INDIVIDUAL GRAPH VARIANTS
 # MODELS 
