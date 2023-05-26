f6e4901c45d492e750ecd66ab20553b140bb5849
Daniel Hulse <daniel.e.hulse@nasa.gov>
2023-05-26 15:48:09 -0700

    stochastic modelling notebook now up-to-date
    - Updated syntax and docs in stochastic_modelling_notebook
    - Fix to resilience_factor_comparison given how Results are structured

--- fmdtools/analyze/result.py
+++ fmdtools/analyze/result.py
@@ -630,11 +630,11 @@ class Result(UserDict):
         nomendclass = endclasses.pop('nominal')
         if not no_diff: 
             if as_ind:
-                difference = {scen:bool(nan_to_x(ec[metric], nan_as))-bool(nan_to_x(nomendclass[metric], nan_as)) for scen, ec in endclasses.items()}
+                difference = {scen:bool(nan_to_x(ec.endclass[metric], nan_as))-bool(nan_to_x(nomendclass[metric], nan_as)) for scen, ec in endclasses.items()}
             else:
-                difference = {scen:nan_to_x(ec[metric], nan_as)-nan_to_x(nomendclass[metric], nan_as) for scen, ec in endclasses.items()}
+                difference = {scen:nan_to_x(ec.endclass[metric], nan_as)-nan_to_x(nomendclass[metric], nan_as) for scen, ec in endclasses.items()}
         else:           
-            difference = {scen:nan_to_x(ec[metric], nan_as) for scen, ec in endclasses.items()}
+            difference = {scen:nan_to_x(ec.endclass[metric], nan_as) for scen, ec in endclasses.items()}
             if as_ind:
                 difference = {scen:np.sign(metric) for scen,metric in difference.items()}
         return difference
