351c2eab784e8ff79a5a3fd86d752bb3475992f1
Daniel Hulse <daniel.e.hulse@nasa.gov>
2023-06-08 18:31:04 -0700

    Adapted testing approach to skip slow notebooks
    - notebooks to run now in fast/slow notebooks list in run_all_tests
    - added some example commands for running just unittests, notebook tests, etc

--- run_all_tests.py
+++ run_all_tests.py
@@ -8,6 +8,42 @@ import pytest
 
 if __name__=="__main__":
     # requires pytest, nbmake, pytest-html
-    retcode = pytest.main(["--html=pytest_report.html", "--nbmake"])
+    #retcode = pytest.main(["--html=pytest_report.html", "--nbmake"])
     
-    #retcode = pytest.main(["--html=pytest_report.html", "--continue-on-collection-errors"])
+    fast_notebooks = ["examples/asg_demo/Action Sequence Graph.ipynb",
+                      "examples/eps/EPS_Example_Notebook.ipynb", 
+                      "examples/pump/Pump_Example_Notebook.ipynb",
+                      "examples/pump/Stochastic_Modelling.ipynb",
+                      "examples/rover/Approach_Use-Cases.ipynb",
+                      "examples/rover/Model_Structure_Visualization_Tutorial.ipynb",
+                      "examples/rover/Nominal_Approach_Use-Cases.ipynb",
+                      "examples/rover/Rover Setup Notebook.ipynb",
+                      "examples/tank/Tank_Analysis.ipynb"
+                      ]
+    
+    # for testing notebooks during development:
+    retcode = pytest.main(["--nbmake", *fast_notebooks])
+    
+    slow_notebooks = ["examples/multirotor/Demonstration.ipynb",
+                      "examples/multirotor/Multirotor Optimization.ipynb",
+                      "examples/pump/AST_Sampling.ipynb",
+                      "examples/pump/Optimization.ipynb",
+                      "examples/pump/Parallelism_Tutorial.ipynb",
+                      "examples/pump/IDETC Results/IDETC_Figures.ipynb",
+                      "examples/rover/degradation_modelling/Degradation Modelling Notebook.ipynb",
+                      "examples/rover/fault_sampling/Rover Mode Notebook.ipynb",
+                      "examples/rover/HFAC_Analyses/HFAC Analyses.ipynb",
+                      "examples/rover/HFAC_Analyses/IDETC_Human_Paper_Analysis.ipynb",
+                      "examples/rover/optimization/Rover Response Optimization.ipynb",
+                      "examples/rover/optimization/Search Comparison.ipynb",
+                      "examples/tank/Tank_Optimization.ipynb"]
+    
+    # for testing longer-running notebooks
+    #retcode = pytest.main(["--nbmake", *slow_notebooks])
+    
+    # for testing all unittests
+    #retcode = pytest.main()
+    
+    
+    # for creating comprehensive test report:
+    #retcode = pytest.main(["--html=pytest_report.html", "--nbmake", "--continue-on-collection-errors"])
