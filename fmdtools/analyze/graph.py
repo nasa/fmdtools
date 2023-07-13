f33d7e91668eed41f236f8c180b38fd1666ae397
Daniel Hulse <daniel.e.hulse@nasa.gov>
2023-07-13 16:32:41 -0700

    define.mode bugfixes
    - File now mostly PEP8 compliant
    - Added some error-handling for when mode is not defined in the subclass

--- fmdtools/analyze/graph.py
+++ fmdtools/analyze/graph.py
@@ -1031,13 +1031,14 @@ class Graph(object):
 
     def find_high_degree_nodes(self, p=90):
         """
-        Determines highest degree nodes, up to percentile p, in graph representation of model mdl.
-        
+        Determines highest degree nodes, up to percentile p, in graph representation of
+        model mdl.
+
         Parameters
         ----------
         p : int (optional)
             percentile of degrees to return, between 0 and 100
-        
+
         Returns
         -------
         highDegreeNodes : list of high degree nodes in format (node,degree)
@@ -1062,7 +1063,8 @@ class Graph(object):
                 highDegreeNodes.append(sortedNodes[i])
         return highDegreeNodes
 
-    def plot_high_degree_nodes(self, p=90, title='', node_kwargs={'node_color': 'red'}, **kwargs):
+    def plot_high_degree_nodes(self, p=90, title='', node_kwargs={'node_color': 'red'},
+                               **kwargs):
         """
         Plots high-degree nodes using self.draw()
 
@@ -1092,14 +1094,15 @@ class Graph(object):
     def calc_robustness_coefficient(self, trials=100, seed=False):
         """
         Computes robustness coefficient of graph representation of model mdl.
-        
+
         Parameters
         ----------
-        trials : int 
-            number of times to run robustness coefficient algorithm (result is averaged over all trials)
+        trials : int
+            number of times to run robustness coefficient algorithm
+            (result is averaged over all trials)
         seed : int
             optional seed to instantiate test with
-        
+
         Returns
         -------
         RC : robustness coefficient
@@ -1109,7 +1112,7 @@ class Graph(object):
             rng = np.random.default_rng(seed=seed)
         else:
             rng = np.random.default_rng()
-        
+
         trialsRC = list()
         for itr in range(trials):
             tmp = g.copy()
@@ -1129,7 +1132,7 @@ class Graph(object):
     def plot_degree_dist(self):
         """
         Plots degree distribution of graph representation of model mdl.
-        
+
         Returns
         -------
         fig : matplotlib figure
@@ -1144,10 +1147,11 @@ class Graph(object):
         maxFreq = max(freq)
         freqint = list(range(0, maxFreq+1))
         degreeint = list(range(min(degrees), math.ceil(max(degrees))+1))
-        degreesSet = set(degrees)  # TODO degreeSet looks to be not used, consider removing - JLO
+        # TODO degreeSet looks to be not used, consider removing - JLO
+        degreesSet = set(degrees)
         degreesUnique = list(degrees)
         numDegreesUnique = len(degreesUnique)
-        
+
         fig = plt.figure()
         plt.hist(degrees, bins=np.arange(numDegreesUnique)-0.5)
         plt.xticks(degreeint)
@@ -1158,10 +1162,11 @@ class Graph(object):
         plt.show()
         return fig
 
-    def sff_model(self, endtime=5, pi=.1, pr=.1, num_trials=100, start_node='random', error_bar_option='off'):
+    def sff_model(self, endtime=5, pi=.1, pr=.1,
+                  num_trials=100, start_node='random', error_bar_option='off'):
         """
         susc-fix-fail model.
-        
+
         Parameters
         ----------
         endtime: int
@@ -1170,17 +1175,16 @@ class Graph(object):
             infection (failure spread) rate
         pr : float
             recovery (fix) rate
-        num_trials : int 
+        num_trials : int
             number of times to run the epidemic model, default is 100
-        error_bar_option : str 
+        error_bar_option : str
             option for plotting error bars (first to third quartile), default is off
         start_node : str
             start node to use in the trial. default is 'random'
-        
+
         Returns
         -------
         fig: plot of susc, fail, and fix nodes over time
-        
         """
         g = self.g
         if start_node == 'random':
@@ -1199,19 +1203,25 @@ class Graph(object):
         num_susc_average = data_average(num_susc_all_trials)
         num_fail_average = data_average(num_fail_all_trials)
         num_fix_average = data_average(num_fix_all_trials)
-       
+
         fig = plt.figure()
         time_list = range(0, endtime+1)
         if error_bar_option == 'on':
-            num_susc_lower_error, num_susc_upper_error = data_error(num_susc_all_trials, num_susc_average)
-            num_fail_lower_error, num_fail_upper_error = data_error(num_fail_all_trials, num_fail_average)
-            num_fix_lower_error, num_fix_upper_error = data_error(num_fix_all_trials, num_fix_average)
+            num_susc_lower_error, num_susc_upper_error = data_error(num_susc_all_trials,
+                                                                    num_susc_average)
+            num_fail_lower_error, num_fail_upper_error = data_error(num_fail_all_trials,
+                                                                    num_fail_average)
+            num_fix_lower_error, num_fix_upper_error = data_error(num_fix_all_trials,
+                                                                  num_fix_average)
             num_susc_asymmetric_error = [num_susc_lower_error, num_susc_upper_error]
             num_fail_asymmetric_error = [num_fail_lower_error, num_fail_upper_error]
             num_fix_asymmetric_error = [num_fix_lower_error, num_fix_upper_error]
-            plt.errorbar(time_list, num_susc_average, yerr=num_susc_asymmetric_error, fmt='-o', label='Susceptible')
-            plt.errorbar(time_list, num_fail_average, yerr=num_fail_asymmetric_error, fmt='-o', label='Failed')
-            plt.errorbar(time_list, num_fix_average, yerr=num_fix_asymmetric_error, fmt='-o', label='Fixed')
+            plt.errorbar(time_list, num_susc_average,
+                         yerr=num_susc_asymmetric_error, fmt='-o', label='Susceptible')
+            plt.errorbar(time_list, num_fail_average,
+                         yerr=num_fail_asymmetric_error, fmt='-o', label='Failed')
+            plt.errorbar(time_list, num_fix_average,
+                         yerr=num_fix_asymmetric_error, fmt='-o', label='Fixed')
         else:
             plt.plot(time_list, num_susc_average, label='Susceptible')
             plt.plot(time_list, num_fail_average, label='Failed')
@@ -1227,7 +1237,7 @@ class Graph(object):
 def sff_one_trial(start_node_selected, g, endtime=5, pi=.1, pr=.1):
     """
     Calculates one trial of the sff model
-    
+
     Parameters
     ----------
     start_node_selected : str
@@ -1256,7 +1266,7 @@ def sff_one_trial(start_node_selected, g, endtime=5, pi=.1, pr=.1):
         time = time + 1
         new_exposed_nodes = []
         for i in range(0, len(fail_nodes)):
-            n = list(g.neighbors(fail_nodes[i])) 
+            n = list(g.neighbors(fail_nodes[i]))
             new_exposed_nodes.extend(n)
         ri_list = [rng.random() for iter in range(len(new_exposed_nodes))]
         new_fail_nodes = []
@@ -1288,12 +1298,14 @@ def sff_one_trial(start_node_selected, g, endtime=5, pi=.1, pr=.1):
         num_fix.append(len(fix_nodes))
     return num_susc, num_fail, num_fix
 
+
 def data_average(data):
     """Averages each column in data"""
     list_average = []
-    for i in range(0,len(data[0])):
+    for i in range(0, len(data[0])):
         list_average.append(sum(x[i] for x in data)/len(data))
     return list_average
+
 
 def data_error(data, average):
     """
@@ -1336,12 +1348,15 @@ def gv_import_check():
         from graphviz import Digraph, Graph
     except ImportError as error:
         print(error.__class__.__name__ + ": " + error.message)
-        raise Exception("GraphViz not installed. Please see:\n https://pypi.org/project/graphviz/ \n https://www.graphviz.org/download/")
+        raise Exception("GraphViz not installed. Please see:",
+                        "\n https://pypi.org/project/graphviz/",
+                        "\n https://www.graphviz.org/download/")
     return Digraph, Graph
 
 
 class GraphInteractor:
-    """A simple interactive graph for consistent node placement, etc--used in set_pos to set node positions"""
+    """A simple interactive graph for consistent node placement,
+    etc.--used in set_pos to set node positions"""
     showverts = True
     epsilon = 0.2  # max pixel distance to count as a vertex hit
 
@@ -1876,7 +1891,8 @@ class ModelTypeGraph(ModelGraph):
         rg = self.g.copy()
         for node in g.nodes:
             if g.nodes[node]['level'] == 2:
-                faulty = any({fxn for fxn, m in g.nodes[node]['faults'].items() if m not in [['nom'], []]})
+                faulty = any({fxn for fxn, m in g.nodes[node]['faults'].items()
+                              if m not in [['nom'], []]})
                 rg.nodes[node]['faulty'] = faulty
             if g.nodes[node]['level'] >= 2:
                 degraded = g.nodes[node]['states'] != nomg.nodes[node]['states']
@@ -1886,7 +1902,7 @@ class ModelTypeGraph(ModelGraph):
     def set_pos(self, auto=True, **pos):
         if auto:
             self.pos = nx.multipartite_layout(self.g, 'level')
-        super().set_pos(auto=False, **pos) 
+        super().set_pos(auto=False, **pos)
 
     def draw_graphviz(self, layout="dot", ranksep='2.0', **kwargs):
         return super().draw_graphviz(layout=layout, ranksep=ranksep, **kwargs)
@@ -1980,7 +1996,7 @@ class MultiFlowGraph(Graph):
 
 
 class CommsFlowGraph(MultiFlowGraph):
-    def __init__(self, flow, include_glob=False, ports_only=False, 
+    def __init__(self, flow, include_glob=False, ports_only=False,
                  get_states=True, get_indicators=True, time=0.0):
         """
         Creates a graph representation of the CommsFlow (assuming no additional locals)
