d5dedb7b12593fe1dbd08999f767d611ecc7dee0
Daniel Hulse <daniel.e.hulse@nasa.gov>
2023-11-12 21:31:32 -0800

    Adding tests for Drone to example
    - Basic search tests added for Drone
    - Variables now nan at initialization (instead of 0.0)

--- examples/multirotor/test_multirotor.py
+++ examples/multirotor/test_multirotor.py
@@ -5,48 +5,73 @@ Created on Wed Dec 14 13:51:18 2022
 @author: dhulse
 """
 import unittest
-from examples.multirotor.opt_drone_rural import opt_prob, x_to_rcost, x_to_ocost
-from examples.multirotor.drone_mdl_rural import Drone
-import multiprocessing as mp
+from examples.multirotor.drone_mdl_rural import Drone, DroneParam
+from fmdtools.sim.sample import ParameterDomain
+from fmdtools.sim.search import ParameterProblem
+import numpy as np
+
+bats = ['monolithic', 'series-split', 'parallel-split', 'split-both']
+
+def bat_var_map(x):
+    return (bats[x], )
+
+linarchs = ['quad', 'hex', 'oct']
+
+def line_arch_map(x):
+    return (linarchs[x], )
+
+expd1 = ParameterDomain(DroneParam)
+expd1.add_variable("phys_param.bat", var_map=bat_var_map, var_lim=(0, 3))
+expd1.add_variable("phys_param.linearch", var_map=line_arch_map, var_lim=(0, 2))
+
+
+ex_soc_opt = ParameterProblem(Drone(), expd1, 'nominal', track=None)
+ex_soc_opt.add_result_objective("f1", "store_ee.s.soc", time=16)
+
+
 
 class DroneTests(unittest.TestCase):
-    def test_interface_values(self):
-        
-        testvalues = [[0,0, 50, 0,0], [0,2, 100, 1,1],[2,2, 150, 1,1]]
-        # NOTE: because there is a fault in the nominal sim that triggers the resilience policy
-        # the value [2,2, 50, 0,0] will give inconsistent results, since the operational model
-        # doesn't have a consistent resilience policy in that case
-        for testvalue in testvalues:
-        
-            rcost_manual = x_to_rcost(testvalue[:2], [testvalue[2]], testvalue[3:], faultmodes='store_ee')
-            rcost_int = opt_prob.cr(testvalue)
-            self.assertAlmostEqual(rcost_manual/10000, rcost_int/10000, 1)
-    def test_sim_types(self):
-        # TODO: Investigate discrepancy between values
-        testvalue= [0,2, 100, 1,1]
-        #rcost_manual = x_to_rcost(testvalue[:2], [testvalue[2]], testvalue[3:], faultmodes='store_ee')
-        
-        opt_prob.update_sim_options("rcost", staged=False, pool=mp.Pool(4))
-        rcost_int = opt_prob.cr(testvalue)
-        #self.assertAlmostEqual(rcost_manual/10000, rcost_int/10000, 1)
-        
-        opt_prob.update_sim_options("rcost", staged=True, pool=mp.Pool(4))
-        rcost_int = opt_prob.cr(testvalue)
-        #self.assertAlmostEqual(rcost_manual/10000, rcost_int/10000, 1)
-        
-        opt_prob.update_sim_options("rcost", staged=True, pool=False) 
-        rcost_int = opt_prob.cr(testvalue)
+    def test_param_domain_1(self):
+        """Test that mapped parameters become expected output parameters."""
+        p_0_0 = expd1(0, 0)
+        self.assertEqual(p_0_0.phys_param.bat, 'monolithic')
+        self.assertEqual(p_0_0.phys_param.linearch, 'quad')
 
-        
-        #self.assertAlmostEqual(rcost_manual/10000, rcost_int/10000, 1)
-        
+    def test_param_domain_2(self):
+        """Test that mapped parameters become expected output parameters."""
+        p_1_2 = expd1(1, 2)
+        self.assertEqual(p_1_2.phys_param.bat, 'series-split')
+        self.assertEqual(p_1_2.phys_param.linearch, 'oct')
+
+    def test_set_constraints_1(self):
+        """Test that mapped parameters result in expected set constraints."""
+        sc = expd1.get_set_constraints(0, 0)
+        self.assertFalse(sc[0])
+        self.assertFalse(sc[1])
+
+    def test_set_constraints_2(self):
+        """Test that mapped parameters result in expected set constraints."""
+        sc1 = expd1.get_set_constraints(4, 1)
+        self.assertTrue(sc1[0])
+        self.assertFalse(sc1[1])
+
+    def test_sim_mdl(self):
+        """Test that Problem tracking options are used (only needed hist/res gotten)."""
+        res, hist = ex_soc_opt.sim_mdl(1,1)
+        self.assertEqual(len(res), 1)
+        self.assertTrue('t16p0.store_ee.s.soc' in res)
+        self.assertEqual(len(hist), 1)
+        self.assertEqual(hist.time[-1], 16.0)
+
+    def test_objectives(self):
+        """Test that increasing architecture weight decreases soc objective."""
+        oldobj = np.inf
+        for i in range(2):
+            obj = ex_soc_opt.f1(0, i)
+            self.assertLess(obj, oldobj)
+            oldobj = obj
+
 
 if __name__ == '__main__':
-    
     unittest.main()
-    
-    #suite = unittest.TestSuite()
-    #suite.addTest(DroneTests("test_sim_types"))
-    #runner = unittest.TextTestRunner()
-    #runner.run(suite)
-        
+
