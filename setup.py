e97b6ddd7a6e2153c077f4b210a438c6be5f1fc9
Daniel Hulse <daniel.e.hulse@nasa.gov>
2024-04-06 14:39:37 -0700

    Migrated to pyproject.toml
    - setup.py now replaced by pyproject.toml
    - Added documented way to check version
    - Fixes: RAD-251

--- setup.py
+++ setup.py
@@ -1,36 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-required = ["scipy",
-            "tqdm",
-            "networkx",
-            "numpy",
-            "matplotlib",
-            "pandas",
-            "ordered-set",
-            "dill",
-            "recordclass",
-            "pytest",
-            "graphviz",
-            "shapely"]
-
-setuptools.setup(
-    name="fmdtools",
-    version="2.0-rc-2",
-    author="Daniel Hulse",
-    author_email="daniel.e.hulse@nasa.gov",
-    description="System resilience modelling, simulation, and assessment in Python",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/nasa/fmdtools",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved ",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.9',
-    install_requires=required,
-)
+setuptools.setup()
