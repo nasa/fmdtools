b174523e02703abccb4397583af47ca2abf1734b
lukmanqw <94932662+lukmanqw@users.noreply.github.com>
2024-04-15 09:58:21 -0700

    Base Skeleton

--- Troupe_skeleton_model.py
+++ Troupe_skeleton_model.py
@@ -15,10 +15,13 @@ from fmdtools.define.container.mode import Mode
 from fmdtools.define.container.state import State
 from fmdtools.define.container.parameter import Parameter
 from fmdtools.define.flow.base import Flow
+from fmdtools.define.flow.multiflow import MultiFlow
+from fmdtools.analyze.graph import FunctionArchitectureGraph, FunctionArchitectureFlowGraph
+from fmdtools.analyze.graph import FunctionArchitectureFxnGraph, FunctionArchitectureTypeGraph
 
 # DEFINE PARAMETERS
 
-class MapParams (CoordsParam):
+class EnvironmentParams (CoordsParam):
     """
     Define the map parameters (default is 100x100 grid where each point is 0.1m).
 
@@ -41,18 +44,33 @@ class MapParams (CoordsParam):
     y_size: int = 100
     blocksize: float = 0.1
 
-
-    num_occupied: int = 3000
+    num_occupied: int = 1000
     feature_explored: tuple = (bool, False)
     feature_occupied: tuple = (bool, False)
     point_start: tuple = (0, 0)
     collect_all_explored: tuple = ("explored", True)
     collect_all_occupied: tuple = ('occupied', True)
 
-class Map(Coords):
+class GroundControlParams(Parameter):
+    """
+    Define the parameter inputs from ground control at various timesteps.
+
+    Features/Collections
+    -------
+    destination: tuple
+        coordinate for the destination
+    alternate_destination: tuple
+        coordinate for an alternate destination in case primary destination is not reachable
+    """
+
+    destination: tuple = (9.9, 9.9)
+    alternate_desitnation: tuple = (9.9, 8)
+#DEFINE FLOWS
+
+class EnvironmentGrid(Coords):
     """Define the map. occupied and unoccupied points."""
 
-    container_p = MapParams
+    container_p = EnvironmentParams
 
     def init_properties(self, *args, **kwargs):
         """Randomly allocate the occupied points"""
@@ -72,34 +90,183 @@ class EnvironmentState(State):
     safe: bool = True
     
 
-class RoverEnvironment(Map):
+class RoverEnvironment(Environment):
     """ Rover environment with occupiable and unoccupiable spaces."""
-    container_p = MapParams
-    coords_c = Map
+    container_p = EnvironmentParams
+    coords_c = EnvironmentGrid
     container_s = EnvironmentState
 
-    
 
-# class Rover(FunctionArchitecture):
-#     __slots__=()
-#     default_sp={}
-#     def init_architecture(self, **kwargs):
-#         #Flows
-#         self.add_flow("location_pose")
-#         self.add_flow("environment")
-#         self.add_flow("electrical_energy")
-#         self.add_flow("map")
-#         self.add_flow("user_input")
-#         #Functions
-#         self.add_fxn("navigation", GenericFxn, "location_pose", "electrical_energy", "environment", "user_input")
-#         self.add_fxn("sensing", GenericFxn, "location_pose", "electrical_energy")
-#         self.add_fxn("mapping", GenericFxn, "environment", "electrical_energy", "map")
-#         self.add_fxn("communication", GenericFxn, "map", "electrical_energy", "user_input")
-#         self.add_fxn("power_supply", GenericFxn, "electrical_energy")
-# mdl = Rover()
+class EEState(State):
+    """Electricity state (voltage v and amperage a)."""
+
+    v: float = 0.0
+    a: float = 0.0
+
+class EE(Flow):
+    """Electricity flow."""
+
+    __slots__ = ()
+    container_s = EEState
+
+class CommsState(State):
+    '''Communication variables between the rover and ground control
+    
+    Feilds
+    ------
+    end_point: tuple 
+        destination point
+    new_desitnation: bool
+        if a new destination is possible
+    map: RoverEnvironment
+        areas mapped by the rover
+    comms_to_ground: str
+        communication from the rover to ground control
+    '''
+    end_point: tuple = (0,0)
+    new_destination: bool = True
+    map: RoverEnvironment = {}
+
+class Comms(Flow):
+    '''Communications flow'''
+    __slots__ = ()
+    container_s = CommsState
+
+class LocationPosState(State):
+    '''Rover location and Pose
+    
+    Feilds
+    ------
+    curr_point: tuple 
+        current_location
+    velocity: list
+        velocity vector with i and j compoenents 
+    '''
+    curr_point: tuple = (0,0)
+    velocity: list = (0,0)
+
+class LocationPos(MultiFlow):
+    '''Flow for the Locationa and Pose of the Rover '''
+    __slots__ = ()
+    container_s = LocationPosState
+
+    
+# DEFINE FUNCTIONS
+class SupplyPowerState(State):
+    """
+    State of power.
+
+    Fields
+    ------
+    charge: float
+        State of charge (percentage).
+    power: float
+        Power output (percentage of soc).
+    """
+
+    charge: float = 100.0
+    power: float = 0.0
+
+class SupplyPowerMode(Mode):
+    """
+    Possible modes for Supply Power function.
+
+    Modes
+    -------
+    no_charge : Fault
+        Battery is out of charge.
+    short: Fault
+        There is a short.
+    supply: Mode
+        supply power
+    charge: Mode
+        charge battery
+    standby: Mode
+        power supply is in stand by
+    off: Mode
+        power supply is off
+    """
+    fm_args = ("no_charge", "short")
+    opermodes = ("off", "supply", "charge", "standby")
+    mode: str =  "off"
+
+class SupplyPower(Function):
+    """Rover power supply."""
+
+    __slots__ = ("ee", "comms")
+    container_s = SupplyPowerState
+    container_m = SupplyPowerMode
+    flow_ee = EE
+    flow_comms = Comms
+
+
+    
+class CommunicateMode(Mode):
+    fm_args = ("packet_not_sent", "loss_from_ground", "wrong_packet_sent")
+
+class Communicate(Function):
+    __slots__ = ("ee", "comms")
+    container_m = CommunicateMode
+    flow_ee = EE
+    flow_comms = Comms
+
+class SenseMode(Mode):
+    fm_args = ("too_late",)
+
+class Sense(Function):
+    __slots__ = ("ee", "location_pose", "environment")
+    container_m = SenseMode
+    flow_ee = EE
+    flow_location_pose = LocationPos
+    flow_environment = RoverEnvironment
+
+class MapMode(Mode):
+    fm_args = ("no_obstacle_detection", "wrong_obstacle_detection", "ghose_obstacle_detection", "no_own_map")
+
+class Map(Function):
+    __slots__ = ("ee", "comms", "environment")
+    container_m = MapMode
+    flow_ee = EE
+    flow_comms = Comms
+    flow_environment = RoverEnvironment
+
+class NavigateMode(Mode):
+    fm_args = ("no_obstacle_detection", "wrong_obstacle_detection", "ghose_obstacle_detection", "no_own_map")
+
+class Navigate(Function):
+    __slots__ = ("ee", "comms", "environment", "location_pose")
+    container_m = NavigateMode
+    flow_ee = EE
+    flow_comms = Comms
+    flow_environment = RoverEnvironment
+    flow_location_pose = LocationPos
+
+class Rover(FunctionArchitecture):
+    __slots__=()
+    default_sp = dict(end_time=150,
+                    phases=(("start", 0, 30), ("end", 31, 150)))
+    def init_architecture(self, **kwargs):
+        #Flows
+        self.add_flow("location_pose", LocationPos)
+        self.add_flow("environment", RoverEnvironment)
+        self.add_flow("ee", EE)
+        self.add_flow("comms", Comms)
+        #Functions
+        self.add_fxn("communicate", Communicate, "ee", "comms")
+        self.add_fxn("supply_power", SupplyPower, "ee", "comms") 
+        self.add_fxn("map", Map, "environment", "ee") 
+        self.add_fxn("sense", Sense, "location_pose", "ee")
+        self.add_fxn("navigate", Navigate, "location_pose", "ee", "environment", "comms")
+        self.add_fxn("communicate", Communicate, "ee", "comms")
+        
+
 
 if __name__ == "__main__":
-    x = Map()
+    mdl = Rover()
+    x = EnvironmentGrid()
     x.show_collection('all_occupied', label = False)
-    x.show_property('occupied',)
-    
+    x.show('explored', collections = {'all_occupied':{'label': False}})
+    x.show_property('occupied')
+    mtg = FunctionArchitectureTypeGraph(mdl)
+    fig, ax = mtg.draw()
+    dot = mtg.draw_graphviz()
