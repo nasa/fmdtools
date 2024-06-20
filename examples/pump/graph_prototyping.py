e6a90af71ad73ddb27537547cc37008beab97c58
Daniel Hulse <daniel.e.hulse@nasa.gov>
2024-06-20 16:08:13 -0700

    More graph work
    - Can now get flexible roles using get_roles
    - Flexible roles now denoted as edges instead of a dict
    - Added determination of aggregation vs containment

--- examples/pump/graph_prototyping.py
+++ examples/pump/graph_prototyping.py
@@ -9,86 +9,129 @@ from fmdtools.analyze.graph.model import ModelGraph
 from fmdtools.analyze.graph.model import set_block_node
 from fmdtools.define.flow.base import Flow
 from fmdtools.define.object.base import BaseObject
+from fmdtools.define.container.base import BaseContainer
 import networkx as nx
 import inspect
+
+
+def get_obj_name(obj, role):
+    if isinstance(obj, BaseObject):
+        name = obj.name
+    elif isinstance(obj, BaseContainer):
+        name = role
+    elif hasattr(obj, '__name__'):
+        name = obj.__name__
+    else:
+        raise Exception("Unknown name for object" + str(obj))
+    return name
+
+
+def get_edge_type(baseobj, obj):
+    if isinstance(obj, BaseObject):
+        if obj.root == baseobj.name:
+            edgetype = 'containment'
+        elif isinstance(obj, Flow):
+            edgetype = 'flow'
+        else:
+            edgetype = 'aggregation'
+    elif inspect.ismethod(obj):
+        if isinstance(baseobj, dict):
+            edgetype = "aggregation"
+        if obj.__self__.name == baseobj.name:
+            edgetype = 'containment'
+        else:
+            edgetype = 'aggregation'
+    elif isinstance(obj, BaseContainer):
+        edgetype = 'containment'
+    else:
+        raise Exception("Unsupported object: " + str(obj))
+    return edgetype
+
+
+def get_node_type(obj):
+    if isinstance(obj, BaseObject) or isinstance(obj, BaseContainer):
+        nodetype = obj.get_typename()
+    else:
+        nodetype = obj.__class__.__name__
+    return nodetype
+
+
+def add_base_node(g, obj, rolename='base'):
+    classname = obj.__class__.__name__
+    name = get_obj_name(obj, rolename)
+    nodetype = get_node_type(obj)
+    g.add_node(name, nodetype=nodetype, classname=classname)
+
+
+def add_role_node(g, baseobj, basename, roleobj, rolename):
+    add_base_node(g, roleobj, rolename=rolename)
+    add_edge(g, baseobj, basename, roleobj, rolename)
+
+
+def add_edge(g, baseobj, basename, roleobj, rolename):
+    name = get_obj_name(roleobj, rolename)
+    edgetype = get_edge_type(baseobj, roleobj)
+    g.add_edge(basename, name, edgetype=edgetype, role=rolename)
+
+
+def add_sub_nodes(g, obj):
+    for rolename, roleobj in obj.get_roles_as_dict(flex_prefixes=True).items():
+        add_role_node(g, obj, obj.name, roleobj, rolename)
+
+
+def set_node_states(g, obj, rolename, time=None):
+    if isinstance(obj, BaseObject):
+        set_block_node(g, obj, obj.name, time=time)
+    elif inspect.ismethod(obj):
+        g.nodes[rolename]['condition'] = obj()
+    else:
+        g.nodes[rolename]['obj'] = obj
 
 
 class RoleGraph(ModelGraph):
 
     def nx_from_obj(self, mdl):
         g = nx.DiGraph()
-        basename = mdl.name
-        g.add_node(basename, nodetype=mdl.get_typename(),
-                   classname=mdl.__class__.__name__)
-        for role in mdl.get_roles():
-            roleobj = getattr(mdl, role)
-            classname = roleobj.__class__.__name__
-            if isinstance(roleobj, Flow):
-                rolename = roleobj.name
-                g.add_node(rolename, nodetype=roleobj.get_typename(),
-                           classname=classname)
-                g.add_edge(basename, rolename, edgetype="flow", role=role)
-            else:
-                g.add_node(role, nodetype=roleobj.get_typename(), classname=classname)
-                g.add_edge(basename, role, edgetype="containment", role=role)
+        add_base_node(g, mdl)
+        add_sub_nodes(g, mdl)
         return g
 
     def set_nx_states(self, mdl, **kwargs):
         for role, roleobj in mdl.get_roles_as_dict().items():
-            if isinstance(roleobj, Flow):
-                set_block_node(self.g, roleobj, roleobj.name, time=self.time)
-            else:
-                self.g.nodes[role]['obj'] = roleobj
+            set_node_states(self.g, roleobj, role, time=self.time)
 
 
 class ArchRoleGraph(RoleGraph):
 
-    def nx_from_obj(self, mdl, with_flow_edges=False):
+    def nx_from_obj(self, mdl, flow_edges=True, cond_edges=True):
         g = RoleGraph.nx_from_obj(self, mdl)
         for flex_role in mdl.flexible_roles:
-            g.add_node(flex_role, nodetype="dict", classname='dict')
-            g.add_edge(mdl.name, flex_role, edgetype="containment",
-                       role=flex_role)
             role_objs = mdl.get_flex_role_objs(flex_role)
             for objname, obj in role_objs.items():
-                if isinstance(obj, BaseObject):
-                    g.add_node(obj.name, nodetype=obj.get_typename(),
-                               classname=obj.__class__.__name__)
-                    g.add_edge(flex_role, obj.name, edgetype="containment", role=objname)
-                elif inspect.ismethod(obj):
-                    g.add_node(obj.__name__, nodetype='Condition', classname='method')
-                    g.add_edge(flex_role, obj.__name__, edgetype="aggregation", role=objname)
-                    g.add_edge(obj.__self__.name, obj.__name__, edgetype='containment',
-                               role=obj.__self__.name)
-                if with_flow_edges and hasattr(obj, 'flows'):
-                    flowdict = obj.get_roles_as_dict('flow')
-                    for locflowname, flowobj in flowdict.items():
-                        g.add_edge(obj.name, flowobj.name, edgetype="flow",
-                                   role=locflowname)
+                if cond_edges and inspect.ismethod(obj):
+                    baseobj = obj.__self__
+                    basename = get_obj_name(baseobj, 'na')
+                    add_edge(g, baseobj, basename, obj, objname)
+                if flow_edges and hasattr(obj, 'flows'):
+                    for locflowname, flowobj in obj.get_roles_as_dict('flow').items():
+                        add_edge(g, obj, objname, flowobj, locflowname)
         return g
 
-    def set_nx_states(self, mdl, **kwargs):
-        super().set_nx_states(mdl, **kwargs)
-        for flex_role in mdl.flexible_roles:
-            role_objs = mdl.get_flex_role_objs(flex_role)
-            for objname, obj in role_objs.items():
-                if isinstance(obj, BaseObject):
-                    set_block_node(self.g, obj, obj.name, time=self.time)
 
+from examples.pump.ex_pump import Pump, ImportEE
 
-from examples.pump.ex_pump import Pump
-
-rg = ArchRoleGraph(Pump(), with_flow_edges=True)
+rg = ArchRoleGraph(Pump(), flow_edges=True)
 rg.set_node_labels(title2='classname', subtext='s')
 rg.set_edge_labels(title='role')
 rg.draw()
 
 rg2 = RoleGraph(Pump().fxns['import_ee'])
-rg2.set_node_labels(title2='classname', subtext='obj')
+rg2.set_node_labels(title2='classname')
 rg2.set_edge_labels(title='role')
 rg2.draw()
 
 from examples.asg_demo.demo_model import Human
 
-rg3 = ArchRoleGraph(Human(), with_flow_edges=True)
+rg3 = ArchRoleGraph(Human(), flow_edges=True)
 rg3.draw()
+mdl = Pump()
--- fmdtools/define/architecture/action.py
+++ fmdtools/define/architecture/action.py
@@ -44,6 +44,7 @@ class ActionArchitecture(Architecture):
     per_timestep = False
     default_track = ('acts', 'flows', 'active_actions', 'i')
     flexible_roles = ['flows', 'acts', 'conds']
+    roletypes = ['container', 'flow', 'act', 'cond']
     rolename = 'aa'
 
     def __init__(self, **kwargs):
