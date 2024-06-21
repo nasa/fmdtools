d14c92cb172b0679ac3c278442d9385883b1bd83
Daniel Hulse <daniel.e.hulse@nasa.gov>
2024-06-20 17:05:53 -0700

    Experimenting with hierarchical graphs

--- examples/pump/graph_prototyping.py
+++ examples/pump/graph_prototyping.py
@@ -15,6 +15,21 @@ import inspect
 
 
 def get_obj_name(obj, role):
+    """
+    Get the name of an object to be graphed.
+
+    Parameters
+    ----------
+    obj : object
+        Object to be graphed (BaseObject, BaseContainer, or other).
+    role : str
+        Role the object plays in the larger system. Determines the name of Containers.
+
+    Returns
+    -------
+    name : str
+        Name of the object.
+    """
     if isinstance(obj, BaseObject):
         name = obj.name
     elif isinstance(obj, BaseContainer):
@@ -27,6 +42,21 @@ def get_obj_name(obj, role):
 
 
 def get_edge_type(baseobj, obj):
+    """
+    Get the edge type connecting two objects.
+
+    Parameters
+    ----------
+    baseobj : object
+        Object at the base of the edge (e.g., thing contain-ing or aggregate-ing).
+    obj : object
+        Object at the end of the edge (e.g., thing being contained).
+
+    Returns
+    -------
+    edgetype : str
+        edgetype label to give the edge (e.g., containment, flow, aggregation, etc).
+    """
     if isinstance(obj, BaseObject):
         if obj.root == baseobj.name:
             edgetype = 'containment'
@@ -49,6 +79,19 @@ def get_edge_type(baseobj, obj):
 
 
 def get_node_type(obj):
+    """
+    Get the type of an object to attach to a node.
+
+    Parameters
+    ----------
+    obj : object
+        Object to represent as a node.
+
+    Returns
+    -------
+    nodetype : str
+        Node type (e.g., Function, State, Component, etc).
+    """
     if isinstance(obj, BaseObject) or isinstance(obj, BaseContainer):
         nodetype = obj.get_typename()
     else:
@@ -56,7 +99,19 @@ def get_node_type(obj):
     return nodetype
 
 
-def add_base_node(g, obj, rolename='base'):
+def add_node(g, obj, rolename='base'):
+    """
+    Add a node to a graph.
+
+    Parameters
+    ----------
+    g : nx.Graph
+        Networkx graph to add to.
+    obj : object
+        Object to add to the graph.
+    rolename : str, optional
+        role of the graph in the larger system (if not base). The default is 'base'.
+    """
     classname = obj.__class__.__name__
     name = get_obj_name(obj, rolename)
     nodetype = get_node_type(obj)
@@ -64,22 +119,85 @@ def add_base_node(g, obj, rolename='base'):
 
 
 def add_role_node(g, baseobj, basename, roleobj, rolename):
-    add_base_node(g, roleobj, rolename=rolename)
+    """
+    Attach the sub-object of a base object.
+
+    Parameters
+    ----------
+    g : nx.Graph
+        Networkx graph to add to.
+    baseobj : object
+        Object that the object is within.
+    basename : str
+        Name of the base object.
+    roleobj : object
+        Object to add.
+    rolename : str
+        Name of the object in the context of the base object.
+    """
+    add_node(g, roleobj, rolename=rolename)
     add_edge(g, baseobj, basename, roleobj, rolename)
 
 
 def add_edge(g, baseobj, basename, roleobj, rolename):
+    """
+    Add an edge from a base object to another object.
+
+    Parameters
+    ----------
+    g : nx.Graph
+        Networkx graph to add to.
+    baseobj : object
+        Object that the object is within.
+    basename : str
+        Name of the base object.
+    roleobj : object
+        Object to add.
+    rolename : str
+        Name of the object in the context of the base object.
+    """
     name = get_obj_name(roleobj, rolename)
     edgetype = get_edge_type(baseobj, roleobj)
     g.add_edge(basename, name, edgetype=edgetype, role=rolename)
 
 
-def add_sub_nodes(g, obj):
-    for rolename, roleobj in obj.get_roles_as_dict(flex_prefixes=True).items():
+def add_sub_nodes(g, obj, recursive=False):
+    """
+    Add the objects contained in the given object to the graph.
+
+    Sub-objects are in the objects roles (e.g. added as container_x, flow_y).
+
+    Parameters
+    ----------
+    g : nx.Graph
+        Networkx graph to add to.
+    obj : object
+        Object to find the objects in.
+    """
+    for rolename, roleobj in obj.get_roles_as_dict(flex_prefixes=True,
+                                                   with_prefix=True).items():
         add_role_node(g, obj, obj.name, roleobj, rolename)
+        if recursive and isinstance(roleobj, BaseObject):
+            add_sub_nodes(g, roleobj, recursive=recursive)
 
 
 def set_node_states(g, obj, rolename, time=None):
+    """
+    Attach stateful attributes to the given node.
+
+    Used to determine faults and degradations in scenario visualization.
+
+    Parameters
+    ----------
+    g : nx.Graph
+        Networkx graph to add to.
+    obj : object
+        Object to get the states from.
+    rolename : str
+        Name of the object in the larger system (if the node is not a BaseObject).
+    time : float, optional
+        Time to get the states from. The default is None.
+    """
     if isinstance(obj, BaseObject):
         set_block_node(g, obj, obj.name, time=time)
     elif inspect.ismethod(obj):
@@ -92,12 +210,12 @@ class RoleGraph(ModelGraph):
 
     def nx_from_obj(self, mdl):
         g = nx.DiGraph()
-        add_base_node(g, mdl)
+        add_node(g, mdl)
         add_sub_nodes(g, mdl)
         return g
 
     def set_nx_states(self, mdl, **kwargs):
-        for role, roleobj in mdl.get_roles_as_dict().items():
+        for role, roleobj in mdl.get_roles_as_dict(with_prefix=True).items():
             set_node_states(self.g, roleobj, role, time=self.time)
 
 
@@ -130,8 +248,21 @@ rg2.set_node_labels(title2='classname')
 rg2.set_edge_labels(title='role')
 rg2.draw()
 
-from examples.asg_demo.demo_model import Human
+from examples.asg_demo.demo_model import Human, HazardModel
 
 rg3 = ArchRoleGraph(Human(), flow_edges=True)
 rg3.draw()
 mdl = Pump()
+
+# looking at hierarchy
+g = nx.DiGraph()
+add_node(g, mdl)
+add_sub_nodes(g, mdl, recursive=True)
+from fmdtools.analyze.graph.base import Graph
+Graph(g).draw()
+
+g = nx.DiGraph()
+mdl = HazardModel()
+add_node(g, mdl)
+add_sub_nodes(g, mdl, recursive=True)
+Graph(g).draw_graphviz()
