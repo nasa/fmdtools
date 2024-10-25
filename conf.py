f05e7f1d684bba29a30c423bf5f20a1c85044ff4
Daniel Hulse <daniel.e.hulse@nasa.gov>
2024-10-25 09:44:05 -0700

    Update version number
    - 2.0.4 will be used for release

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ copyright = '2024, United States Government as represented by the Administrator 
 author = 'fmdtools developers'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.3'
+release = '2.0.4'
 version = release
 
 
