b3d79858747ea91a7aba88623755070fbdbb4e4a
zain khatri <zainkhatri2560@gmail.com>
2025-08-21 15:20:24 -0700

    License header fixes

--- fmdtools/analyze/graph/__init__.py
+++ fmdtools/analyze/graph/__init__.py
@@ -6,7 +6,7 @@ Package for graph representations and analysis of model structures.
 Copyright © 2024, United States Government, as represented by the Administrator
 of the National Aeronautics and Space Administration. All rights reserved.
 
-The ""Fault Model Design tools - fmdtools version 2"" software is licensed
+The "Fault Model Design tools - fmdtools version 2" software is licensed
 under the Apache License, Version 2.0 (the "License"); you may not use this
 file except in compliance with the License. You may obtain a copy of the
 License at http://www.apache.org/licenses/LICENSE/2.0. 
