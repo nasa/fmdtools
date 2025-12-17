1e1f6db48178e7e10f658c412b3846b6ff6a32df
Daniel Hulse <daniel.e.hulse@nasa.gov>
2025-12-17 11:46:06 -0800

    Merge in airspacelib
    - Add in airspacelib commit history
    - Add in airspacelib files

---
+++ examples/airspacelib/__init__.py
@@ -0,0 +1,5 @@
+import airspacelib.base as base
+import airspacelib.contingencymanagement as contingencymanagement
+import airspacelib.wildfireresponse as wildfireresponse
+
+__all__ = ['base', 'contingencymanagement', 'wildfireresponse']
---
+++ examples/airspacelib/wildfireresponse/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 13 17:20:30 2025
+
+@author: dhulse
+"""
+
+import airspacelib.wildfireresponse.aircraft as aircraft
+import airspacelib.wildfireresponse.environment as environment
+import airspacelib.wildfireresponse.wildfiresim as wildfiresim
+
+
+__all__ = ['aircraft', 'environment', 'wildfiresim']
