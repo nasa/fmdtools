202a25ca88d83a15a4944f4c01282e518c0b1439
Daniel Hulse <daniel.e.hulse@nasa.gov>
2026-01-22 14:01:30 -0800

    Un-hide conftest.py
    Conftest.py was being hidden from git--likely the cause of our testing issues.

--- run_all_tests.py
+++ run_all_tests.py
@@ -7,8 +7,6 @@ Requires pytest, nbmake, pytest-html, pytest-cov.
 """
 import pytest
 import argparse
-import sys
-import os
 
 
 def main(testtype="doctests", auto_build_reports=False):
@@ -16,18 +14,8 @@ def main(testtype="doctests", auto_build_reports=False):
     if auto_build_reports:
         rep_args = ["--cov-report", "html:auto"]
 
-    print("os cwd: "+os.getcwd())
-    print("sys paths: "+"\n ".join(sys.path))
-
-    try:
-        pytest.main(["-v"])
-                     #"--testtype="+testtype,
-                     #"--auto_build_reports="+str(auto_build_reports), *rep_args])
-    except:
-        print("os cwd: "+os.getcwd())
-        print("sys paths: "+"\n ".join(sys.path))
-        import conftest
-
+    pytest.main(["--testtype="+testtype,
+                 "--auto_build_reports="+str(auto_build_reports), *rep_args])
 
 if __name__ == "__main__":
 
