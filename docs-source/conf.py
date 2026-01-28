7de2e015875d99a7713ff35af422c7ca7d3e5882
Daniel Hulse <daniel.e.hulse@nasa.gov>
2026-01-28 14:25:12 -0800

    Migration of file structure to src format
    - fmdtools folder now in src/fmdtools
    - test files moved into /tests

--- docs-source/conf.py
+++ docs-source/conf.py
@@ -15,7 +15,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../src'))
+sys.path.append(os.path.abspath("../")
 
 
 # -- Project information -----------------------------------------------------
