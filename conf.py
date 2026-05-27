eb471aec617e4806ba69c484b27bf153cf6fd0ab
Daniel Hulse <daniel.e.hulse@nasa.gov>
2026-05-27 11:31:37 -0700

    Bump version to start test/release process
    - version: 2.3.3->2.4.0
    - Python version now listed as 3.12-3.14 (test setup update in progress)
    - see: RAD-565

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ copyright = '2024, United States Government as represented by the Administrator 
 author = 'fmdtools developers'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.3'
+release = '2.4.0'
 version = release
 
 
